6642b529d001bce721c87cb0349db88f4beed911
steph_ano <stephrvq@gmail.com>
2026-05-18 20:36:06 -0500

    fix: actualizar Newtonsoft.Json a 13.0.1 y Npgsql a 8.0.3 para resolver CVEs

--- src/backend-dotnet/VortexFlow.Application/obj/Debug/net8.0/VortexFlow.Application.AssemblyInfo.cs
+++ src/backend-dotnet/VortexFlow.Application/obj/Debug/net8.0/VortexFlow.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VortexFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e391d78144033e8de8f61a708c5811ed67012f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VortexFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VortexFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
